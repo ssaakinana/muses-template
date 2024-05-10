53ed087e82a0393622b4cc5b94de6735cc8b1a53
o513n <166586319+o513n@users.noreply.github.com>
2024-05-10 12:13:17 +0900

    jsコメント追加

--- js-template/index.js
+++ js-template/index.js
@@ -1,15 +1,21 @@
-'use strict';
+"use strict";
 
-document.addEventListener('DOMContentLoaded', async () => {
+// ページの読み込みが完了した時に実行される処理
+document.addEventListener("DOMContentLoaded", async () => {
+  // セッションストレージからユーザー名を取得
   const username = sessionStorage.username;
+  // もしユーザー名が存在しない場合はログインページにリダイレクトし、アラートを表示する
   if (!username) {
-    window.alert('ログインしてください');
-    location.href = 'login.html';
+    window.alert("ログインしてください");
+    location.href = "login.html";
   }
-  document.querySelector('#user_name span').textContent = username;
+  // ユーザー名を表示する要素にユーザー名を設定する
+  document.querySelector("#user_name span").textContent = username;
 
-  const res = await fetch('data.json');
-  const obj = await res.json();
-
-  document.querySelectorAll('span.unread').forEach((el) => (el.textContent = obj.list.length));
+  // data.jsonからデータを取得し、未読件数を表示する
+  const res = await fetch("data.json"); // データを取得するためのリクエストを送信
+  const obj = await res.json(); // レスポンスをJSON形式に変換
+  document
+    .querySelectorAll("span.unread")
+    .forEach((el) => (el.textContent = obj.list.length)); // 未読件数を表示する
 });
